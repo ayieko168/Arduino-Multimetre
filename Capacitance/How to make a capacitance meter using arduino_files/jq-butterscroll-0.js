6307287dca914ce905a981a8ec62993f92898e1d
ayieko168 <antonyalen1960@gmail.com>
2019-11-17 01:50:14 +0300

    First Commit.

---
+++ Capacitance/How to make a capacitance meter using arduino_files/jq-butterscroll-0.js
@@ -0,0 +1,137 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/main_template.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>A estudiar - Toca ser ingenioso</title>
+<!-- InstanceEndEditable -->
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62541190-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<link rel="shortcut icon" href="images/icon.ico"/>
+
+<link href="CSS/main.css" rel="stylesheet" type="text/css" />
+
+
+
+<link rel="stylesheet" href="css/butterscroll.css" type="text/css" />
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="js/jq-butterscroll-0.0.1.js"></script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.5&appId=249946393271";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+<!--[if lte IE 7]>
+<style>
+.content { margin-right: -1px; } /* this 1px negative margin can be placed on any of the columns in this layout with the same corrective effect. */
+ul.nav a { zoom: 1; }  /* the zoom property gives IE the hasLayout trigger it needs to correct extra whiltespace between the links */
+</style>
+<![endif]-->
+</head>
+
+<body>
+
+<video autoplay loop poster="" id="bgvid">
+    <source src="images/BG8.webm" type="video/webm">
+   
+</video>
+
+
+
+
+<div class="container">
+
+
+
+  <div class="header">
+  <!-- InstanceBeginEditable name="cabecera" -->
+   <div class="barra_fixa">
+
+<div class="barra_fixa1">
+<a href="#article-1"><img src="images/gotop.png" width="90%" height="90%" alt="Top"></a>
+</div>
+<div class="barra_fixa1">
+<a href="#article-2"><img src="images/mid.png" width="90%" height="90%" alt="Mid"></a>
+</div>
+<div class="barra_fixa1">
+<a href="#article-3"><img src="images/Bot.png" width="90%" height="90%" alt="Bot"></a>
+</div>
+
+
+<script>
+            (function (jQuery) {
+                jQuery('a').butterScroll({
+					speed: 900,
+                    scroll  : 500,
+					toplink : true
+                });
+            })(jQuery);
+        </script>
+</div>
+ <article id="article-1"></article>
+ 
+
+<?php include("includes/menus.php"); ?>
+   <?php include("includes/menu_top.php"); ?>
+
+  <!-- InstanceEndEditable -->
+  <!-- end .header --></div>
+  <div class="sidebar1"><!-- InstanceBeginEditable name="barra_lateral" -->
+      <div class="barra_superior"></div>
+     
+    <div class="fb-page" data-href="https://www.facebook.com/Electronoobs" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/Electronoobs"><a href="https://www.facebook.com/Electronoobs">Electronoobs</a></blockquote></div></div>
+ 
+    <tex1><br/> <a href="https://es.wikipedia.org/wiki/Albert_Einstein">Albert Einstein</a> joven físico desconocido, empleado en la Oficina de Patentes de Berna, publicó su teoría de la relatividad especial.</tex1><br/><br/>
+    <tex1>El primer <a href="https://es.wikipedia.org/wiki/Circuito_integrado#Historia">circuito integrado</a> fue desarrollado en 1959 por el ingeniero <a href="https://es.wikipedia.org/wiki/Jack_S._Kilby">Jack S. Kilby1</a> (1923-2005) pocos meses después de haber sido contratado por la firma <a href="https://es.wikipedia.org/wiki/Texas_Instruments">Texas Instruments</a>. </tex1>
+    <br/>
+    <div class="lo_nuevo">
+    <img src="images/Menu/noticias.png" width="90%" height="90%" alt="Noticias">
+    </div>
+  <!-- InstanceEndEditable -->
+  <!-- end .sidebar1 --></div>
+  <div class="content"><!-- InstanceBeginEditable name="contenido" -->
+
+
+  <article id="article-2"></article>
+  <h1>Electromagnetismo</h1>
+ 
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  <br/>
+  
+    
+  <!-- InstanceEndEditable -->
+  <!-- end .content --></div>
+  <div class="footer"><!-- InstanceBeginEditable name="pie" -->
+    ELECTRONOOBS<br/>
+    2015
+     <article id="article-3"></article>
+  <!-- InstanceEndEditable -->
+  <!-- end .footer --></div>
+  <!-- end .container --></div>
+   
+
+</body>
+<!-- InstanceEnd --></html>
